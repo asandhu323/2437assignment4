4599a00c43e49ced3d036d171adedefddc412036
Avneet Sandhu <aksx97@gmail.com>
2021-05-18 12:52:51 -0400

    login

--- app.js
+++ app.js
@@ -26,6 +26,69 @@ app.get('/', function (req, res) {
 
 });
 
+// No longer need body-parser!
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/authenticate', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+
+    let results = authenticate(req.body.email, req.body.password,
+        function(rows) {
+            if(rows == null) {
+                res.send({ status: "fail", msg: "User account not found." });
+            } else {
+                req.session.loggedIn = true;
+                req.session.email = rows.email;
+                req.session.save(function(err) {
+                })
+                res.send({ status: "success", msg: "Logged in." });
+            }
+    });
+
+});
+
+
+function authenticate(email, pwd, callback) {
+
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'accounts'
+    });
+
+    connection.query(
+      "SELECT * FROM user WHERE email = ? AND password = ?", [email, pwd],
+      function (error, results) {
+        if (error) {
+            throw error;
+        }
+
+        if(results.length > 0) {
+            return callback(results[0]);
+        } else {
+            return callback(null);
+        }
+
+    });
+
+}
+
+app.get('/logout', function(req,res){
+    req.session.destroy(function(error){
+        if(error) {
+            console.log(error);
+        }
+    });
+    res.redirect("/profile");
+})
+
+let port = 8000;
+app.listen(port, function () {
+    console.log('Listening on port ' + port + '!');
+})
+
 
 // RUN SERVER
 let port = 8000;
